f81e9c3c2241c9bbd7817be3b0fd982744e0f95d
İbrahim EDİZ <edizdesign@gmail.com>
2023-03-30 14:04:57 +0000

    30032023_3

--- Documents/folder.py
+++ Documents/folder.py
@@ -1,6 +1,6 @@
 import os
 liste = os.listdir("Exercises/")
-fileName = "01_01_try_except"
+fileName = "02_01_funds"
 for item in liste:
     with open(f"Exercises/{item}/{fileName}.py","a+") as dosya:
         pass
