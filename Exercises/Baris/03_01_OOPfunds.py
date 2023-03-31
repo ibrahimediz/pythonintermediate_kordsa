c4a55a135737455a183ffd0b8201a20c2c05a018
İbrahim EDİZ <edizdesign@gmail.com>
2023-03-31 13:55:36 +0000

    310320231655

--- Exercises/Baris/03_01_OOPfunds.py
+++ Exercises/Baris/03_01_OOPfunds.py
@@ -1,4 +1,18 @@
 
-Class Cat:
-        def __init
-        
+class Cat:
+        def __init__(self,name,spiece,age,gender):
+                self.name = name
+                self.spiece = spiece
+                self.age = age
+                self.gender = gender
+
+        def sayMyName(self):
+                print(self.name)
+
+        def makeNoise(self):
+                print(self.name ,"Meow") 
+
+cat1 =Cat("Boncuk","Tekir","3","Female") 
+cat2 =Cat("Paşa","Scottish","3","Male")      
+
+cat1.sayMyName()
